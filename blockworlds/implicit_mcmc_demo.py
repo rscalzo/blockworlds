db26095490e37d4528ff292a695246543f182a59
Richard Scalzo <richard.a.scalzo@gmail.com>
2021-01-20 17:46:16 +1100

    Bulk uncommitted changes, mostly about fold demo model sampling

--- blockworlds/implicit_mcmc_demo.py
+++ blockworlds/implicit_mcmc_demo.py
@@ -9,19 +9,22 @@ the same infrastructure I've been using in the notebooks, put into production.
 
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.special as sp
 from blockworlds import profile_timer, DiscreteGravity
 from blockworlds import baseline_tensor_mesh, survey_gridded_locations
-from implicit import gen_two_fault_model_demo
+from blockworlds himport gen_two_fault_model_demo, gen_fold_model_demo
 from riemann import Sampler, Model
 from riemann.proposals.randomwalk import AdaptiveMetropolisRandomWalk as AMRW
+from riemann.samplers.ptsampler import PTSampler
 import pickle
+import emcee
 
 
 # Reproducible random numbers
 np.random.seed(413)
 
 # True values of parameters for various demo models
-model_pars = np.array([
+fault_model_pars = np.array([
     # Original graben model
     [3.0, 350.0, 2.5, 190.0, 2.0,           # layer densities and thicknesses
      -400.0, 0.0, +20.0, 0.0, -220.0,       # 1st fault x0, y0, theta, phi, s
@@ -56,10 +59,55 @@ model_pars = np.array([
      -300.0, 0.0, 40.0, 0.0, 80.0],
 ])
 
+# Labels for plots
+fault_model_parnames = [
+    "Basement Density (g cm$^{-3}$)",
+    "Layer 1 Thickness (m)",
+    "Layer 1 Density (g cm$^{-3}$)",
+    "Layer 2 Thickness (m)",
+    "Layer 2 Density (g cm$^{-3}$)",
+    "Fault 1 Contact X Position (m)",
+    "Fault 1 Contact Y Position (m)",
+    "Fault 1 Polar Elevation (deg)",
+    "Fault 1 Polar Azimuth (deg)",
+    "Fault 1 Dip-Direction Slip (m)",
+    "Fault 2 Contact X Position (m)",
+    "Fault 2 Contact Y Position (m)",
+    "Fault 2 Polar Elevation (deg)",
+    "Fault 2 Polar Azimuth (deg)",
+    "Fault 2 Dip-Direction Slip (m)",
+]
+
 # Characteristic initial-guess step sizes for MCMC
-stepsizes = np.array([0.1, 100, 0.01, 100, 0.01,
-                      1.0, 1.0, 1.0, 1.0, 100,
-                      1.0, 1.0, 1.0, 1.0, 100])
+stepsizes = np.array([0.1, 10.0, 0.02, 10.0, 0.02,
+                      0.2, 0.2, 3.0, 3.0, 30.0,
+                      0.2, 0.2, 3.0, 3.0, 30.0])
+
+# Mark Lindsay's fold model
+fold_model_pars = np.array([
+    [3.0, 350.0, 2.5, 190.0, 2.0,
+     0.0, 0.0, 0.0, 0.0, 1000.0, 100.0,
+     250.0, 0.0, -205.0, 0.0, 200.0]
+])
+
+fold_model_parnames = [
+    "Basement Density (g cm$^{-3}$)",
+    "Layer 1 Thickness (m)",
+    "Layer 1 Density (g cm$^{-3}$)",
+    "Layer 2 Thickness (m)",
+    "Layer 2 Density (g cm$^{-3}$)",
+    "Fold 1 Axis Elevation (deg)",
+    "Fold 1 Axis Azimuth (deg)",
+    "Fold 1 Pitch Angle (deg)",
+    "Fold 1 Phase Angle (deg)",
+    "Fold 1 Wavelength (m)",
+    "Fold 1 Amplitude (m)",
+    "Fault 1 Contact X Position (m)",
+    "Fault 1 Contact Y Position (m)",
+    "Fault 1 Polar Elevation (deg)",
+    "Fault 1 Polar Azimuth (deg)",
+    "Fault 1 Dip-Direction Slip (m)",
+]
 
 
 # ============================================================================
@@ -69,17 +117,32 @@ stepsizes = np.array([0.1, 100, 0.01, 100, 0.01,
 
 class GeoModel(Model):
 
-    def __init__(self, history, fwdmodel, dsynth, sigdata):
+    def __init__(self, history, fwdmodel, dsynth, sigdata, alpha=1e+4):
         # Set baseline attributes
         self.history = history
-        self.fwdmodel = fwdmodel
         self.dsynth = dsynth
         self.sigdata = sigdata
+        self.alpha = alpha
+        # Pre-compute constant norm for t-distribution likelihood
+        self.beta = sigdata**2 * alpha
+        self.tnorm = (sp.gammaln(self.alpha + 0.5) - sp.gammaln(self.alpha)
+                      - 0.5*np.log(2*np.pi) + self.alpha*np.log(self.beta))
         # Set forward model to represent history
-        self.fwdmodel.gfunc = history.rockprops
+        self.set_fwdmodel(fwdmodel)
         # Turn full anti-aliasing on; match h to grid resolution
+        self.set_antialiasing(True)
+
+    def set_fwdmodel(self, fwdmodel):
+        self.fwdmodel = fwdmodel
+        self.fwdmodel.gfunc = self.history.rockprops
         mesh = self.fwdmodel.mesh
-        self.h = np.exp(np.mean(np.log([mesh.hx, mesh.hy, mesh.hz])))
+        self.hmesh = np.exp(np.mean(np.log([mesh.hx, mesh.hy, mesh.hz])))
+
+    def set_antialiasing(self, antialiasing):
+        if antialiasing:
+            self.h = self.hmesh
+        else:
+            self.h = 0.001*self.hmesh
 
     def log_likelihood(self, theta):
         # Load this parameter vector into the history and calculate gravity
@@ -87,9 +150,17 @@ class GeoModel(Model):
         dpred = self.fwdmodel.calc_gravity(self.h)
         resids = dpred - self.dsynth
         resids = resids - resids.mean()
-        # Independent Gaussian likelihood with variance sigdata**2
-        return -0.5 * np.sum(resids ** 2 / self.sigdata ** 2 +
-                             np.log(2 * np.pi * self.sigdata ** 2))
+        # if self.alpha > 1000:
+        if self.alpha > 100:
+            # Independent Gaussian likelihood with variance sigdata**2
+            return -0.5 * np.sum(resids ** 2 / self.sigdata ** 2 +
+                                 np.log(2 * np.pi * self.sigdata ** 2))
+        else:
+            # Independent t-distribution likelihood with tail weight alpha
+            # and scale parameter sigdata = np.sqrt(beta/alpha)
+            A, B, T = self.alpha, self.beta, self.tnorm
+            return np.sum(-(A + 0.5) * np.log(B + 0.5 * resids**2) + T)
+
 
     def log_prior(self, theta):
         # Load this parameter vector into the history and calculate prior density
@@ -97,37 +168,36 @@ class GeoModel(Model):
         return self.history.logprior()
 
 
-def initialize_geomodel(pars):
+def initialize_geomodel(pars, L, NL, foldhistory=False):
     """
     Initialize a GeoModel instance and get it ready for sampling
     :param pars: parameter vector (see top of file)
     :return: GeoModel instance ready for sampling with riemann
     """
     # Initialize a mesh for forward gravity calculation
-    z0, L, NL = 0.0, 1000.0, 15
-    h = L / NL
+    z0, h = 0.0, L / NL
     print("z0, L, nL, h =", z0, L, NL, h)
     mesh = baseline_tensor_mesh(NL, h, centering='CCN')
     survey = survey_gridded_locations(L, L, 20, 20, z0)
 
     # Initialize a GeoHistory based on the parameters passed in
     history = gen_two_fault_model_demo(pars)
+    history.deserialize(pars)
 
     # Initialize a DiscreteGravity forward model instance
-    fwdmodel = DiscreteGravity(mesh, survey, history.event_list[0])
-    fwdmodel.gfunc = history.event_list[0].rockprops
-    fwdmodel.edgemask = profile_timer(fwdmodel.calc_gravity, h)
+    fwdmodel = DiscreteGravity(mesh, survey, history.rockprops)
 
     # Make some synthetic data based on this history and mesh
     data0 = fwdmodel.calc_gravity(h)
+    np.random.seed(413)
     sigdata = 0.05 * np.std(data0)
     epsilon = sigdata * np.random.normal(size=data0.shape)
     dsynth = data0 + epsilon
 
     # Construct and return a GeoModel
-    return GeoModel(history, fwdmodel, dsynth, sigdata)
+    return GeoModel(history, fwdmodel, dsynth, sigdata, alpha=2.5)
 
-def run_mcmc(model, Nsamp=100000, Nburn=20000, Nthin=100):
+def run_mcmc(model, Nsamp=100000, Nburn=20000, Nthin=100, temper=False):
     """
     Runs the MCMC using riemann.
     :param model: GeoModel instance
@@ -137,11 +207,17 @@ def run_mcmc(model, Nsamp=100000, Nburn=20000, Nthin=100):
     model.history.set_to_prior_draw()
     histpars = model.history.serialize()
     proposal = AMRW(0.1 * np.diag(stepsizes), 100, marginalize=False)
-    sampler = Sampler(model, proposal, np.array(histpars))
+    if temper:
+        sampler = PTSampler(model, proposal, np.array(histpars),
+                            betas=0.5**np.arange(10))
+    else:
+        sampler = Sampler(model, proposal, np.array(histpars))
     profile_timer(sampler.run, Nsamp)
     chain = np.array(sampler._chain_thetas)
     accept_frac = np.mean(chain[1:] - chain[:-1] != 0)
+    tau = emcee.autocorr.integrated_time(chain, quiet=True)
     print("run_mcmc: chain finished; acceptance fraction =", accept_frac)
+    print("run_mcmc:  autocorrelation time = ", tau)
     return chain[Nburn:Nsamp:Nthin]
 
 def gelman_rubin(data, verbose=False):
@@ -188,35 +264,37 @@ def gelman_rubin(data, verbose=False):
 # ============================================================================
 
 
-def run_grid(model, p1_vals, p2_vals, p1_idx, p2_idx):
-    origpars = model.history.serialize()
+def run_grid(my_model, p1_vals, p2_vals, p1_idx, p2_idx):
+    origpars = my_model.history.serialize()
     grid_vals = [ ]
     for p1i in p1_vals:
         for p2i in p2_vals:
             theta = np.array(origpars)
             theta[p1_idx], theta[p2_idx] = p1i, p2i
-            grid_vals.append([p1i, p2i, model.log_posterior(theta)])
-    model.history.deserialize(origpars)
+            grid_vals.append([p1i, p2i, my_model.log_posterior(theta)])
+    my_model.history.deserialize(origpars)
     grid_vals = np.array(grid_vals).T.reshape(3, len(p1_vals), len(p2_vals))
     return grid_vals
 
 def show_contours(xg, yg, Lg, p1_vals, p2_vals, p1_0, p2_0):
     levels = 10**np.arange(-6,0.1)
-    levels = np.log(10)*np.arange(-6,0.1)
+    levels = np.arange(-6,0.1)
     # plt.contourf(xg, yg, np.exp(Lg - Lg.max()), levels=levels)
-    plt.contourf(xg, yg, Lg - Lg.max(), levels=levels)
+    plt.contourf(xg, yg, (Lg - Lg.max())/np.log(10), levels=levels)
     plt.colorbar()
     ax = plt.gca()
     ax.set_xlim(p1_vals.min(), p1_vals.max())
     ax.set_ylim(p2_vals.min(), p2_vals.max())
+    ax.set_title("$\log_{10} P(\\theta|d)/P(\\theta_\mathrm{MAP}|d)$")
 
-def vet_slice(model, z1_idx, z2_idx, zdelt1, zdelt2, Nz):
-    histpars = np.array(model.history.serialize())
+def vet_slice(my_model, z1_idx, z2_idx, zdelt1, zdelt2, Nz):
+    histpars = np.array(my_model.history.serialize())
     z1_0, z2_0 = histpars[z1_idx], histpars[z2_idx]
     z1_vals = np.linspace(z1_0-0.5*zdelt1, z1_0+0.5*zdelt1, Nz)
     z2_vals = np.linspace(z2_0-0.5*zdelt2, z2_0+0.5*zdelt2, Nz)
-    xg, yg, Lg = profile_timer(run_grid, model, z1_vals, z2_vals, z1_idx, z2_idx)
+    xg, yg, Lg = profile_timer(run_grid, my_model, z1_vals, z2_vals, z1_idx, z2_idx)
     show_contours(xg, yg, Lg, z1_vals, z2_vals, z1_0, z2_0)
+    return xg, yg, Lg
 
 
 # ============================================================================
@@ -224,30 +302,185 @@ def vet_slice(model, z1_idx, z2_idx, zdelt1, zdelt2, Nz):
 # ============================================================================
 
 
-def run_experiments():
-    M, Nsamp, Nburn, Nthin = 4, 100000, 20000, 100
+def run_fault_model_sampling():
+    L, NL = 1000.0, 15
+    M, Nsamp, Nburn, Nthin, temper = 4, 100000, 20000, 10, False
     model_chains_h0, model_chains_h1 = [ ], [ ]
-    for pars in model_pars:
+    estimated_model_runtime = M * Nsamp * 360/1e+5
+    if temper:
+        estimated_model_runtime *= 10
+    estimated_total_runtime = 2 * len(fault_model_pars) * estimated_model_runtime
+    print("starting experimental run")
+    print("estimated model run time: {:.2} s".format(estimated_model_runtime))
+    print("estimated total run time: {:.2} s".format(estimated_total_runtime))
+    for i, pars in enumerate(fault_model_pars):
         # Initialize model and grab the mesh cell size
-        model = initialize_geomodel(pars)
-        h = model.h
-        # Turn anti-aliasing off and sample
-        model.h = 0.001 * h
-        chains = np.array([run_mcmc(model, Nsamp, Nburn, Nthin) for i in range(M)])
-        model_chains_h0.append(np.array(chains))
-        print("chains_h0.shape =", chains.shape)
-        print("gelman_rubin(): Rhat = {}".format(gelman_rubin(chains)))
-        # Turn anti-aliasing back on and sample again
-        model.h = h
-        chains = np.array([run_mcmc(model, Nsamp, Nburn, Nthin) for i in range(M)])
+        model = initialize_geomodel(pars, L, NL)
+        basefname = "implicit_{}_chains".format(i)
+        # Sample first with anti-aliasing on
+        print("anti-aliasing ON:  model.h =", model.h)
+        chains = np.array([run_mcmc(model, Nsamp, Nburn, Nthin, temper=temper)
+                           for i in range(M)])
         model_chains_h1.append(np.array(chains))
         print("chains_h1.shape =", chains.shape)
         print("gelman_rubin(): Rhat = {}".format(gelman_rubin(chains)))
-    # Save to pickles for later; most likely faster than re-running!
-    with open("model_chains_h0.pkl", 'wb') as pklfile:
-        pickle.dump(np.array(model_chains_h0), pklfile)
+        with open(basefname + "_h1.pkl", 'wb') as pklfile:
+            pickle.dump(np.array(chains), pklfile)
+        # Turn anti-aliasing back on and sample again
+        model.h *= 0.001
+        print("anti-aliasing OFF:  model.h =", model.h)
+        chains = np.array([run_mcmc(model, Nsamp, Nburn, Nthin, temper=temper)
+                           for i in range(M)])
+        model_chains_h0.append(np.array(chains))
+        print("chains_h0.shape =", chains.shape)
+        print("gelman_rubin(): Rhat = {}".format(gelman_rubin(chains)))
+        # Save to pickles for later; most likely faster than re-running!
+        with open(basefname + "_h0.pkl", 'wb') as pklfile:
+            pickle.dump(np.array(chains), pklfile)
+
     with open("model_chains_h1.pkl", 'wb') as pklfile:
         pickle.dump(np.array(model_chains_h1), pklfile)
+    with open("model_chains_h0.pkl", 'wb') as pklfile:
+        pickle.dump(np.array(model_chains_h0), pklfile)
+
+def run_fold_model_sampling():
+    L, NL = 1000.0, 30
+    M, Nsamp, Nburn, Nthin, temper = 4, 100000, 20000, 10, False
+    model_chains_h0, model_chains_h1 = [ ], [ ]
+    estimated_model_runtime = M * Nsamp * 360/1e+5
+
+def sampling_table():
+    output = ""
+    for i, pars in enumerate(fault_model_pars):
+        basefname = "implicit_{}_chains".format(i)
+        for h in range(2):
+            akalabels = ['   ', '-AA']
+            ext =  "_h{}.pkl".format(h)
+            with open(basefname + ext, 'rb') as pklfile:
+                print("opening: {}".format(basefname+ext))
+                chains = pickle.load(pklfile) # [-1]
+                taus = [emcee.autocorr.integrated_time(ch, quiet=True) for ch in chains]
+                taus = np.array(taus) * 0.01
+                tmean, tmin, tmax = np.mean(taus), np.min(taus), np.max(taus)
+                Rhat = gelman_rubin(chains)
+                Rmean, Rmin, Rmax = np.mean(Rhat), np.min(Rhat), np.max(Rhat)
+                output += (
+                    "    {}{} & {:.1f} ({:.1f}, {:.1f}) $\\times 10^{{3}}$"
+                    " & {:.2f} ({:.2f}, {:.2f}) \\\\\n".format
+                    (i, akalabels[h], tmean, tmin, tmax, Rmean, Rmin, Rmax)
+                )
+    print(output)
+
+def traceplots(basepklfname):
+    """
+    Make some trace plots for the paper
+    :param pklfname: filename for pickled chains
+    :return: N/A
+    """
+    # Chains array is of shape [Nchains, Nsamples, Npars]
+    with open(basepklfname + "_h0.pkl", 'rb') as pklfile:
+        chains_h0 = pickle.load(pklfile)
+    with open(basepklfname + "_h1.pkl", 'rb') as pklfile:
+        chains_h1 = pickle.load(pklfile)
+    for i, ylabel in enumerate(fault_model_parnames):
+        plt.figure(figsize=(5,7))
+        plt.subplot(2, 1, 1)
+        plt.plot(chains_h0[:,:,i].T)
+        plt.title("Chain Traces for Model 5 (Aliased)")
+        plt.xlabel('Sample Number ($\\times 100$)')
+        plt.ylabel(ylabel)
+        plt.subplot(2, 1, 2)
+        plt.plot(chains_h1[:,:,i].T)
+        plt.title("Chain Traces for Model 5 (Anti-Aliased)")
+        plt.xlabel('Sample Number ($\\times 100$)')
+        plt.ylabel(ylabel)
+        plt.subplots_adjust(top=0.92, bottom=0.08, left=0.16, hspace=0.35)
+        plt.savefig("implicit_5_trace_{}.eps".format(i))
+        # plt.show()
+
+def slice_figures(model_idx):
+
+    # Initialize two GeoModels at different resolutions but same parameters
+    L, NL, xmag, Nz = 1000.0, 15, 5, 30
+    geopars = fault_model_pars[model_idx]
+    model = initialize_geomodel(geopars, L, NL)
+    model_hires = initialize_geomodel(geopars, L, NL*xmag)
+    # Extract the forward models from these and make the data agree
+    model.dsynth = model_hires.dsynth
+
+    # Set up the three-column plots
+    fwdmodels = [model.fwdmodel, model_hires.fwdmodel,
+                 model.fwdmodel, model_hires.fwdmodel]
+    akasettings = [False, False, True, True]
+
+    # Actual parameters passed to plotting routine
+    sliceparlist = [
+        (model, 1,  3, 600.0, 200.0, Nz), # widths of layers on top of basement
+        (model, 7, 12,  25.0,  25.0, Nz), # the two fault elevation angles
+        (model, 9, 14, 200.0, 200.0, Nz), # the two fault displacements
+        (model, 3,  7, 200.0,  25.0, Nz), # layer width vs elevation angle
+        (model, 3, 14, 200.0, 200.0, Nz), # layer width vs displacement
+        (model, 7, 14,  25.0, 200.0, Nz), # elevation angle vs displacement
+    ]
+    rowlabels = "abcdef"
+    collabels = ["Coarse Aliased", "Fine Aliased",
+                 "Coarse Anti-Aliased", "Fine Anti-Aliased"]
+
+    # First row should be slices
+    fig = plt.figure(figsize=(14, 3))
+    for i in range(len(fwdmodels)):
+        fm, aka = fwdmodels[i], akasettings[i]
+        model.set_fwdmodel(fm)
+        model.set_antialiasing(aka)
+        profile_timer(fm.calc_gravity, model.h)
+        # Plot the cross-section
+        ax1 = plt.subplot(1, len(fwdmodels), i + 1)
+        fm.plot_model_slice(ax=ax1, axlabels=(i == len(fwdmodels)-1),
+                            grid=(i % 2 == 0))
+        ax1.set_title(collabels[i])
+        ax1.set_xlabel("x (m)")
+        if i == 0:
+            plt.ylabel("z (m)")
+        else:
+            plt.ylabel("")
+
+    # plt.subplots_adjust(top=0.85, bottom=0.15, hspace=0.35, wspace=0.35)
+    plt.subplots_adjust(bottom=0.2, left=0.08, right=0.92, wspace=0.35)
+    figfn = "sliceplots/implicit_{}_slices.eps".format(model_idx)
+    plt.savefig(figfn)
+
+    # Now scan through all the posteriors
+    xg, yg, Lg, args = [ ], [ ], [ ], [ ]
+    for j, slicepars in enumerate(sliceparlist):
+        fig = plt.figure(figsize=(14, 3))
+        for i in range(len(fwdmodels)):
+            fm, aka = fwdmodels[i], akasettings[i]
+            model.set_fwdmodel(fm)
+            model.set_antialiasing(aka)
+            ax = plt.subplot(1, len(fwdmodels), i + 1)
+            xgji, ygji, Lgji = vet_slice(*slicepars)
+            plt.xlabel(fault_model_parnames[slicepars[1]])
+            if i == 0:
+                plt.ylabel(fault_model_parnames[slicepars[2]])
+            args.append(slicepars[1:])
+            xg.append(xgji)
+            yg.append(ygji)
+            Lg.append(Lgji)
+        # plt.subplots_adjust(top=0.85, bottom=0.15, hspace=0.35, wspace=0.35)
+        plt.subplots_adjust(bottom=0.2, left=0.08, right=0.92, wspace=0.35)
+        figfn = "sliceplots/implicit_{}{}_slices.eps".format(
+            model_idx, rowlabels[j])
+        plt.savefig(figfn)
+
+    pklfn = "sliceplots/implicit_{}_slices.pkl".format(model_idx)
+    with open(pklfn, 'wb') as pklfile:
+        pickle.dump([xg, yg, Lg, args], pklfile)
+
 
 if __name__ == "__main__":
-    run_experiments()
+    # for model_idx in range(len(fault_model_pars)):
+    #     slice_figures(model_idx)
+    # profile_timer(run_fault_model_sampling)
+    traceplots("chainplots/amrw_stepsize=v2_thin10_t5/implicit_5_chains")
+    # traceplots("chainplots/amrw_stepsize=v2_thin10_t5/implicit_5_chains")
+
