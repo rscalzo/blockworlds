6368e8008ac0bfe83ee7493f871985562af68809
Richard Scalzo <richard.a.scalzo@gmail.com>
2021-08-13 15:26:28 +1000

    Cleaned up global namespace of package

--- blockworlds/__init__.py
+++
@@ -1,4 +0,0 @@
-from .blockworlds import profile_timer
-from .blockworlds import DiscreteGravity
-from .blockworlds import baseline_tensor_mesh, survey_gridded_locations
-from .implicit import gen_two_fault_model_demo, gen_fold_model_demo
